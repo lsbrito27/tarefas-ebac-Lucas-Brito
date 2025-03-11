e73139906259eb1e44b307a1cdd743de937c84ad
Lucas Brito <lsbrito27@gmail.com>
2025-03-10 21:39:05 -0300

    Upload do exercicio mod8\tarefa_calculo_media

---
+++ mod8/tarefa_calculo_media/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //Recebe as 4 notas do aluno.
+        double nota1 = 5.5;
+        double nota2 = 7.0;
+        double nota3 = 3.5;
+        double nota4 = 8.0;
+
+        //Calcula a media das notas.
+        double media = (nota1 + nota2 +  nota3 + nota4) / 4;
+
+        //Imprime no console a media.
+        System.out.println("A media das notas foi: " + media);
+
+    }
+}
